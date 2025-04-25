f4b14ae5d4294b5b6db89fb3669e3698c6babe78
Abdulkadir Özyurt <abdulkadir.ozyurt@gmail.com>
2025-04-26 02:26:12 +0300

    Implement IBrandService interface in BrandManager class

--- Business/Abstract/IBrandService.cs
+++ Business/Abstract/IBrandService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities.Concrete;
 
 namespace Business.Abstract
 {
     public interface IBrandService
     {
-
+        List<Brand> GetAll();
+        Brand GetById(int brandId);
     }
 }
